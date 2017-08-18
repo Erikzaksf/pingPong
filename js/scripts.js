1e9445b9532d294468273a1c4bf9004f4ce0a1a7
erikzaksf <erik.zak.sf@gmail.com>
2017-08-18 12:04:45 -0700

    add some business logic but not working yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
+  // business logic
+
+var outArray = new Array;
+var tooAdd = new Array;
+var numberInput = new Array;
+var countDown = [];
 
 
+function createString(number) {
+
+
+
+
+
+  do {
+
+ return outArray.push(numberInput);
+  numberInput -= 1 ;
+  }while (numberInput > 1);
+};
 
 
 
@@ -10,8 +28,9 @@
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
-debugger
+
     var numberInput =  $("input.user-input").val();
-    
+    var result = createString(outArray);
+    window.alert(result);
   });
  });
