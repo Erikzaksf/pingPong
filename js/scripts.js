4ab7ed8c608d3235f31e26b34b136a061888563d
erikzaksf <erik.zak.sf@gmail.com>
2017-08-18 15:00:18 -0700

    add more javascript in attempt to return a array

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,34 @@
 var outArray = new Array;
 var tooAdd = new Array;
 var numberInput = new Array;
-var countDown = [];
+var one = 1;
+var highEnd = new Array;
+var result = new Array
+
+//function createArray() {
 
 
-function createString(number) {
+
+//}
+
+function createString() {
+  while (highEnd > one) {
+      one++;
+      return outArray.unshift(one);
+  };
 
 
+//  if (numberInput > 1) {
+//    outArray.push(numberInput);
+//    numberInput -= 1 ;
+//  }
 
-
-
-  do {
-
- return outArray.push(numberInput);
-  numberInput -= 1 ;
-  }while (numberInput > 1);
 };
+//do {
+  //}while (numberInput > 1);
+  //numberInput -= 1 ;
+  //return outArray.push(numberInput);
+//};
 
 
 
@@ -28,9 +41,9 @@ function createString(number) {
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
-
-    var numberInput =  $("input.user-input").val();
-    var result = createString(outArray);
+debugger
+    var highEnd =  $("input.user-input").val();
+    result = createString(highEnd);
     window.alert(result);
   });
  });
